8209fdc2b21219d9ae54acdcc54236d2b36d61db
DenisKovacek <54936220+DenisKovacek@users.noreply.github.com>
2020-02-24 16:10:42 +0100

    Add files via upload

---
+++ SaveWorldTesting/CheckLoginTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaveWorldController;
+using SaveWorldModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveWorldTesting
+{
+    [TestClass]
+    public class CheckLoginTest
+    {
+        [TestMethod]
+        public void CheckLoginData()
+        {
+            UserB user = new UserB();
+            UserCtr userCtr = new UserCtr();
+            Assert.IsNotNull(userCtr.CheckLogin("asdasd@as.dk", "123"));
+        }
+
+    }
+}
---
+++ SaveWorldTesting/CreatingProductTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaveWorldController;
+using SaveWorldModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveWorldTesting
+{
+
+    [TestClass]
+    public class CreatingProductTest
+    {
+        [TestMethod]
+        public void CreatingAProduct()
+        {
+            ProductCtrB productCtr = new ProductCtrB();
+            ProductB prod = new ProductB();
+            prod.ProductName = "umbrella";
+            prod.ProductDescription = "a very nice umbrella";
+            prod.Stock = 10;
+            prod.Price = 10.35M;
+            Assert.IsNotNull(productCtr.CreateProduct(prod));
+        }
+    }
+}
